a9996ad549cad0d7002915ffba76426f38b7227a
TonyZhechev <zhechevtony@gmail.com>
2023-09-21 20:46:17 +0300

    Nested Loops added on 21.9.2023

---
+++ src/NestedLoops/NumberPyramid.java
@@ -0,0 +1,21 @@
+package NestedLoops;
+
+import java.util.Scanner;
+
+public class NumberPyramid {public static void main(String[] args) {
+
+    Scanner scanner = new Scanner( System.in );
+
+    int n = Integer.parseInt(scanner.nextLine());
+    int count = 0;
+
+    for ( int i = 1; i <= n; i++) {
+
+        for ( int k = 1; k <= i; k++ ) {
+            count++;
+            System.out.printf("%d ", count);
+            if (count == n ) return;
+        }
+        System.out.println();
+    }
+}}
