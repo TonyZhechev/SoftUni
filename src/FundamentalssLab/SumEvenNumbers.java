7fc9409a505611b8ded94e311069ffb8b089f031
TonyZhechev <zhechevtony@gmail.com>
2023-10-28 17:53:15 +0300

    Fundamentals exercise added on 28.10.2023

--- src/FundamentalssLab/SumEvenNumbers.java
+++ src/FundamentalssLab/SumEvenNumbers.java
@@ -1,22 +1,15 @@
 package FundamentalssLab;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class SumEvenNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        scanner.nextLine();
 
-        int[] numArr = Arrays.stream(input.split( " ")).mapToInt(e->Integer.parseInt(e)).toArray();
         int sum = 0;
-        for ( int i = 0; i < numArr.length ; i++) {
-            int currentNum= numArr[i];
-            if (currentNum % 2 == 0){
-                sum +=currentNum;
-            }
-            
-        }
         System.out.println(sum);
+
+
     }
 }
